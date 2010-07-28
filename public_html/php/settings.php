eba66ad38ca719c104efb7d481ed02836502bfe8
Jonas Bjork <jonas.bjork@aller.se>
2010-07-28 10:14:25 +0200

    Started to clean up settings.php

--- public_html/php/settings.php
+++ public_html/php/settings.php
@@ -18,10 +18,10 @@ $SETTINGS["KUNDNUMMER_URL"] = "http://www.allersforlag.se/Templates/UserService_
 
 
 //MySQL
-$dbdb =	'krillomera';
-$dbhost = 'localhost';
-$dbuser = 'krillomera';
-$dbpass = 'krillomera';
+$dbdb =	'[SQL_DB]';
+$dbhost = '[SQL_HOST]';
+$dbuser = '[SQL_USER]';
+$dbpass = '[SQL_PASS]';
 
 // Truncate feed items
 define('TRUNCATE_OLDER_THAN', 4);
@@ -35,7 +35,7 @@ define("NO_INTERNET", true);
 define("GOOGLEMAPS_OVERRIDE_NO_INTERNET", false);
 
 //Memcache
-define('MEMCACHE', true); //Sätter på memcache
+define('MEMCACHE', [USE_MEMCACHE]); //Sätter på memcache
 define('MEMCACHE_EXPIRE', (60*5)); //i sekunder innan man tömmer objectet ur DB. 0 = oändlig tid framöver ;) (60*60*24*30) är max man kan ange, dvs 30 dygn
 define('MEMCACHE_SERVER', 'localhost');
 
@@ -58,18 +58,11 @@ define('PDF_TEMPLATE_DIR', ROOT . "/pdfomera/");
 $google_map_api_keys = array(
     'krillomera.se' => 'ABQIAAAAKzMyNmzHpI2ys7Y4yLCUKxTy4MN0sW6gGuXvGNy8gkPzTfAzNBTX7HXHVNDeZ9bpiBeeIiqjxGc89A',
 	'testomera.se' => 'ABQIAAAANKJiM3KX0RgwW7CohhxtexTMcDzwyi0Hm9IHdH_sNWT7RCXriBRqeHrTLsDwDJNVmStXOp1zogUnyA',
-	'localmotiomera' => 'ABQIAAAAthwpzPchZb-OjNhtFyAU3BSZZqeaU-3s5X6tGQynxqy6msFf4hTGO7Yrlwx8zRlO6JkR3LLzc32P4w',
-	'localomera2' => 'ABQIAAAAuMDzylsCMpa8xNliwARAcRTFGTp4vNAWkFcwtEPbyN6x_DGpexRR1mXi4qrQ53r0Hx_9O58_G9HptQ',
 	'trunkomera.se' => 'ABQIAAAAuMDzylsCMpa8xNliwARAcRT_kyFNl-ka9r_oTqT-uZF8Air7lxStmrTx98iSJewQi9Wz9HZj-dLTtQ',
 	'localhost' => 'ABQIAAAAuMDzylsCMpa8xNliwARAcRT2yXp_ZAY8_ufC3CFXhHIE1NvwkxT83aaz0LC6xlEaulYzjESpV-HB4Q',
-	'trunk-locamera.se' => 'ABQIAAAAoKGJwdpOav2ETgqnbjarahSkV6AqBAeUnTI_vPQf_1xiJaon3RS1Wvyc5yeWCzj4dcCTd4uucpkYcQ',
 	'motiomera.local' => 'ABQIAAAANKJiM3KX0RgwW7CohhxtexS1LzYKLbLQjIR8BUgnzcVXnHMhFBRhxYys6eSBfGDz1G7wWynCXYGP5w',
-	'trunk.motiomera' => 'ABQIAAAAoKGJwdpOav2ETgqnbjarahRRMBEu5CALF7xZh5XrDQKcx7HCdhRqsCjsYrui-qJk3N6UAiLA_9fXsg',
 	'motiomera.se' => 'ABQIAAAAoKGJwdpOav2ETgqnbjarahQO7RuXVt7MF_rSjXZeZTDFiOd82xQjxvpURu3QurFzD-6V3p4mcOMujg',
 	'www.motiomera.se' => 'ABQIAAAAoKGJwdpOav2ETgqnbjarahSYvq-FPqK4lLNaOXGWuDfDL82xGRTsfWTH7cJboy2WmrVTUP0RY_wNvQ',
-    'krillomera:8000' => 'ABQIAAAAKzMyNmzHpI2ys7Y4yLCUKxQxZXq48oV33njLpoh52aXFtCGfsBTAwsq_8zddECoVedO_T_KNMkwD9w',
-    'order:8000' => 'ABQIAAAAKzMyNmzHpI2ys7Y4yLCUKxSGGwTHf-R8xkabalYVzie--IfwcxRoAXtWpsYG_1OuOfJJu8ycbC9Bmw',
-    'order' => 'ABQIAAAAKzMyNmzHpI2ys7Y4yLCUKxQmbD_Pu_h58hOAVOSeD2sevjvPcBRG4JHAhDqUJZ56sA3eSDNFtvvCgA',
 );
 
 if (isset($google_map_api_keys[$_SERVER['HTTP_HOST']])) {
