e9296e6a2478d7e959b8215147d698fde32cf14e
Kristian Erendi <krillo@gmail.com>
2011-02-13 21:27:31 +0000

    reclamation fix

--- public_html/actions/reclamation.php
+++ public_html/actions/reclamation.php
@@ -9,9 +9,9 @@ if ( isset($_POST['fid']) && isset($_POST['nbr'])){
   	//successfully sent 
   	//write a record to mm_reclamation 
     $rec = new Reclamation($_POST['fid'], $_POST['nbr']);
-    header("Location: " . $SETTINGS["url"] . "pages/editforetag.php?fid=$fid&tab=5");
+    header("Location: " . $SETTINGS["url"] . "/pages/editforetag.php?fid=$fid&tab=5");
   }else{
-    header("Location: " . $SETTINGS["url"] . "pages/editforetag.php?fid=$fid&tab=5");  	
+    header("Location: " . $SETTINGS["url"] . "/pages/editforetag.php?fid=$fid&tab=5");
   }  
 }
 ?>
