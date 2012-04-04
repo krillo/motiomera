34fea590720ca55bfb57670f3efd57dc16887a24
Kristian Erendi <krillo@gmail.com>
2012-04-04 12:05:16 +0200

    frakt is now 70 kr

--- public_html/php/classes/Order.php
+++ public_html/php/classes/Order.php
@@ -161,7 +161,7 @@ class Order extends Mobject {
           "typ" => "frakt",
           "text" => "Frakt",
           "extra" => " kr ex. moms.",
-          "pris" => 50,
+          "pris" => 70,
           "dagar" => 0,
           "popupid" => 22,
           "public" => TRUE,
