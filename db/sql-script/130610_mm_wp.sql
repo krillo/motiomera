1064f3129a77e7f7cfda617b4df48fdbacd2ac04
Kristian Erendi <krillo@gmail.com>
2013-06-10 18:19:35 +0200

    added table mm_dagbok, ruined nginx-conf...

--- db/sql-script/130610_mm_wp.sql
+++ db/sql-script/130610_mm_wp.sql
@@ -1,3 +1,9 @@
 ALTER TABLE `mm_medlem` ADD `fbid` INT  NULL  DEFAULT NULL  AFTER `id`;
-
-
+CREATE TABLE `mm_dagbok` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `mm_id` int(11) NOT NULL,
+  `kommentar` varchar(140) CHARACTER SET latin1 DEFAULT NULL,
+  `betyg` int(1) DEFAULT NULL,
+  `datum` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
