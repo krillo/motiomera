02d5a612d9c95c18d4477fe8349a33448dc21c86
Kristian Erendi <kristian@reptilo.se>
2013-09-27 06:01:26 +0200

    cleanup of file installningar.php

--- public_html/pages/installningar.php
+++ public_html/pages/installningar.php
@@ -1,12 +1,8 @@
 <?php
-
 require_once($_SERVER["DOCUMENT_ROOT"]."/php/init.php");
-
 Security::demand(USER);
-
 $smarty = new MMSmarty();
 $smarty->assign("pagetitle", "Min profil");
-
 
 $tabs = new TabBox("installningar", 760, null);
 $tabs->addTab("Allm&auml;nt", "allmant");
@@ -19,8 +15,6 @@ if(isset($_GET["tab"]))
 	$tabs->setSelected($_GET["tab"]);
 
 $smarty->assign("tabs", $tabs);
-
-
 $visningsbild = $USER->getVisningsbild();
 $smarty->assign("visningsbild", $visningsbild);
 
@@ -32,13 +26,9 @@ foreach($kommuner as $kommun){
 $sel_kommun = $USER->getKommunId();
 $smarty->assign("opt_kommuner", $opt_kommuner);
 $smarty->assign("sel_kommun", $sel_kommun);
-
 $customVisningsbild = $USER->getCustomVisningsbild();
 $smarty->assign("customVisningsbild", $customVisningsbild);
-
 $unapprovedVisningsbild = $USER->getCustomVisningsbild(false);
-
-
 $smarty->assign("unapprovedVisningsbild", $unapprovedVisningsbild);
 
 
@@ -49,13 +39,6 @@ if($USER->getForetag()){
 	$opt_access["ingen"] = "Inte öppen för någon";
 }
 $smarty->assign("opt_access", $opt_access);
-
 $sel_access = $USER->getAtkomst();
 $smarty->assign("sel_access", $sel_access);
-
-
-
 $smarty->display('installningar.tpl');
-
-
-?>
