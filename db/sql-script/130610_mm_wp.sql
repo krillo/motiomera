e5ab2015823d198cc12fb8503e7fe9d3036eb82e
krillo <krillo@gmail.com>
2014-03-10 08:58:48 +0100

    Update 130610_mm_wp.sql
    kommentarerna hade latin1... ändrade till utf8

--- db/sql-script/130610_mm_wp.sql
+++ db/sql-script/130610_mm_wp.sql
@@ -5,11 +5,11 @@ ALTER TABLE `mm_medlem` ADD `fbid` INT  NULL  DEFAULT NULL  AFTER `id`;
 CREATE TABLE `mm_dagbok` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `mm_id` int(11) NOT NULL,
-  `kommentar` varchar(140) CHARACTER SET latin1 DEFAULT NULL,
+  `kommentar` varchar(140) COLLATE utf8_swedish_ci DEFAULT NULL,
   `betyg` int(1) DEFAULT NULL,
   `datum` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 -- new transparent images
 update mm_lag set bildUrl = replace(bildUrl, 'jpg', 'png') where bildUrl like 'Lag_%';
